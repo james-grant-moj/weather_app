896bf68456625bed314f5ac9a86a47d82d0a85db
james <jamesg@givergy-events.co.uk>
2016-07-05 07:11:38 +0100

    Update main template

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -3,7 +3,7 @@
     angular.module('weather_app').config(function($routeProvider){
         $routeProvider
             .when('/',{
-                templateUrl: '../templates/main.html',
+                templateUrl: './templates/main.html',
                 controller: 'main-controller',
                 controllerAs: 'mainCtrl'
             })
